40eaffb764a950efcb569ac4ba56cdd6207a5d00
lenhatminh10042001 <64116754+lenhatminh10042001@users.noreply.github.com>
2022-08-23 12:13:06 +0700

    Filter places according to UserId Params

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 
@@ -27,12 +28,14 @@ const DUMMY_PLACES = [
       lat: 40.7484405,
       lng: -73.9856644,
     },
-    creator: "u1",
+    creator: "u2",
   },
 ];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES} />;
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
